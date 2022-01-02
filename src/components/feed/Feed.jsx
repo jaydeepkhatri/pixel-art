22e3fc548ef418488e88e9e94a5bedc7c3f1a7ab
Jaydeep Khatri <jaydeep@kukui.com>
2022-01-02 17:38:08 +0530

    ➕ Added gid

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -3,15 +3,24 @@ import './feed.css';
 
 
 function Feed() {
+    const maindivs = [];
+    const innerdivs = [];
+
+    for(let j = 0; j < 32; j++) {
+        innerdivs.push(<div className={"pixel"+j}></div>);
+    }
+
+    for(let i = 0;i < 32; i++){
+        maindivs.push(<div className="row" id={"row"+i}>{innerdivs}</div>);
+    }
+
+
     return(
         <main>
            <div className='boxes-grid'>
-               {
-                Array.from(Array(100).keys())?.map(x=>(
-                    <div className="gr"></div>
-                ))
-               }
+               {maindivs}
            </div>
+           <button onClick={() => alert('Download ho gaya')}>Download</button>
         </main>
     )
 }
