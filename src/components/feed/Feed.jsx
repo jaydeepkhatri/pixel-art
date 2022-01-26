4ae84d2a1eacfc59bef1ea7b4b18f65e942b9e99
Jaydeep Khatri <jaydeep@kukui.com>
2022-01-26 21:01:12 +0530

    Added functionality

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,62 +1,48 @@
 import React, {useEffect, useState} from 'react';
+import html2canvas from 'html2canvas';
 import './feed.css';
 
+const downloadPic = () => {
+    html2canvas(document.querySelector(".boxes-grid")).then(function(canvas) {
+        let lnk = document.createElement('a');
+        lnk.download = "myimage";
+        lnk.href=canvas.toDataURL("image/png;base64");
+        // Download image with Fake link
+        lnk.click();
+    });
+}
 
 function Feed() {
     const maindivs = [];
     const innerdivs = [];
     const [xPos, setxPos] = useState(0);
     const [yPos, setyPos] = useState(0);
+    const [CurrentColor, setCurrentColor] = useState("#F00");
 
     for(let j = 0; j < 32; j++) {
-        innerdivs.push(<div className={"pixel"+j} onMouseOver={() => setxPos(j)}></div>);
+        innerdivs.push(<div className={"pixel"+j} key={j} onMouseEnter={() => setxPos(j)}></div>);
     }
 
     for(let i = 0;i < 32; i++){
-        maindivs.push(<div className="row" id={"row"+i} onMouseOver={() => setyPos(i)}>{innerdivs}</div>);
+        maindivs.push(<div className="row" id={"row"+i} key={i} onMouseEnter={() => setyPos(i)}>{innerdivs}</div>);
     }
+
     
     useEffect(() => {
-        document.querySelectorAll(".row")
-      }, []);
+        document.querySelector("#row"+yPos+" .pixel"+xPos).style.backgroundColor = CurrentColor;
+        document.querySelector(".currentcolor").style.backgroundColor = CurrentColor;
+    }, [xPos, yPos, CurrentColor]);
 
     const colors = [
-        {
-            name: "Light Red",
-            colorhex: "#F78181",
-        },
-        {
-            name: "Orange",
-            colorhex: "#FF8A00",
-        },
-        {
-            name: "Yellow",
-            colorhex: "#F4EB03",
-        },
-        {
-            name: "Green",
-            colorhex: "#6EFF15",
-        },
-        {
-            name: "Teuquilla",
-            colorhex: "#1DFFD6",
-        },
-        {
-            name: "Blue",
-            colorhex: "#1286F0",
-        },
-        {
-            name: "Dark Blue",
-            colorhex: "#200CFF",
-        },
-        {
-            name: "Purple",
-            colorhex: "#D731F2",
-        },
-        {
-            name: "Red",
-            colorhex: "#FF012F",
-        }
+        { name: "Light Red", colorhex: "#F78181" },
+        { name: "Orange", colorhex: "#FF8A00" },
+        { name: "Yellow", colorhex: "#F4EB03" },
+        { name: "Green", colorhex: "#6EFF15" },
+        { name: "Teuquilla",colorhex: "#1DFFD6" },
+        { name: "Blue", colorhex: "#1286F0" },
+        { name: "Dark Blue", colorhex: "#200CFF" },
+        { name: "Purple", colorhex: "#D731F2" },
+        { name: "Red", colorhex: "#FF012F" }
     ];
     return(
         <main className="main">
@@ -64,15 +50,16 @@ function Feed() {
                {maindivs}
            </div>
            <div className="sidebar">
+           <div className="currentcolor"></div>
            <div className="colors">  
            {
                 colors.map((color, index) => (
-                    <div className='color_item' key={index} style={{backgroundColor: color.colorhex}}></div>
+                    <div className='color_item' title={color.name} key={index} onClick={() => setCurrentColor(color.colorhex)} style={{backgroundColor: color.colorhex}}></div>
                 ))
             }
-            <input type="color" style={{backgroundColor: "#ad1d1d"}}  /> 
+            <input type="color" style={{backgroundColor: "#ad1d1d"}} onChange={(e) => setCurrentColor(e.target.value)} /> 
            </div>
-           <button onClick={() => alert('Download ho gaya')}>Download</button>
+           <button onClick={() => downloadPic()}>Download</button>
            <div className='stats'>
                X: {xPos}<br/>
                Y: {yPos}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import { Link } from "react-router-dom";
 import { HiMenuAlt3 } from 'react-icons/hi';
 import './navbar.css'
 
